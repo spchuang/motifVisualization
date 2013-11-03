4ed6839d269143ecf768020f62b3c79cd3c11731
Sam Chuang <sam4609ping1892@gmail.com>
2013-11-03 03:31:56 -0800

    fix small bugs. add deffer to test()

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var centroidWindow = ['1','2','3','4','5','6','7','8','9','10','11','12','13','1
 var template = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,100]
 
 var centroidNubmer = 10;
-var motifNumber = 498;
+var motifNumber = 5;//498;
 var PROCESSING = false;
 var navSelect = -1;
 
@@ -25,6 +25,18 @@ var sub_navigate = {
 			id:'corr_overview', 
 			name: 'Correlation Overview',
 			mainFunc: correlation_overview_graph
+		}],
+	'type_2':[
+		{
+			id:'motif_pattern',
+			name: 'Motif for each celltypes',
+			mainFunc: test
+		},
+		{
+			id:'motif_across_cell',
+			name: 'Motif across celltypes',
+			mainFunc: test
+			
 		}]
 };
 
@@ -32,7 +44,10 @@ var sub_navigate = {
 
 
 function test(){
+ 	var deffered = $.Deferred();
 	console.log("test");
+	deffered.resolve();
+	return deffered;
 }
 $(document).ready(function(){
 	
@@ -211,7 +226,7 @@ function loadMainContent(main_tab){
 				break;
 			}
 		}
-		$("#graph_1_nav li ").removeClass('active');
+		$("#graph_"+index+"_nav li ").removeClass('active');
 		$(this).parent().addClass('active');
 		$("#graph_container div").removeClass('show').addClass("hide");
 		$("#graph_"+index+"_"+(subNavIndex+1)).removeClass("hide").addClass('show');
