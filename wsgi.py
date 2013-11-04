3b7052aed91419acd414234a262f174380f05358
Sam Chuang <sam4609ping1892@gmail.com>
2013-11-04 10:19:05 -0800

    try1

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,23 @@
-import cherrypy
+import cherrypy    
 import os
 from src import app
  
 ROOT_DIR = os.path.join(os.path.abspath("."))
+port = int(os.environ.get("PORT", 5000))
+
 config = {'/':
                 {'tools.staticdir.on': True,
                  'tools.staticdir.dir': ROOT_DIR,
 
                  "server.socket_host": "0.0.0.0",
-        		 "server.socket_port": 5000,		#was 8086
+        		 "server.socket_port": port,		#was 8086
 
                 }
         }
  
 application = cherrypy.tree.mount(app.DataVisual(),config=config)
  
+
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 5000, application, True)
+    run_simple('0.0.0.0', port, application, True)
