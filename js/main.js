131eb4cfc301663d3f4284bad90e7f8f4cbb45c4
Sam Chuang <sam4609ping1892@gmail.com>
2013-11-04 10:22:36 -0800

    change number to original

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ var centroidWindow = ['1','2','3','4','5','6','7','8','9','10','11','12','13','1
 
 var template = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,100]
 */
-var centroidNubmer = 10;
-var motifNumber = 10;
+var centroidNubmer = 100;
+var motifNumber = 498;
 var PROCESSING = false;
 var navSelect = -1;
 
@@ -682,7 +682,7 @@ function get_chart(renderDivID, title, category, hasSeries){
 	var series = [];
 	var yAxis = { 
                 title: {
-                    text: 'correlation Level',
+                    text: 'Level',
                     style: {color: '#4572A7'}
                 }
             };
